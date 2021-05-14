0c6455e9399434de3e532b47ffb70bca2cb19629
Shasha Yan <shashayan@gmail.com>
2021-05-14 13:13:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,8 +2,8 @@ def prime?(num)# Add  code here!
   if num < 0 || num == 0 || num == 1
     false
   else
-    (2..num-1).to_a.all? do |num|
-      num % !num != 1
+    (2..num-1).to_a.all? do |i|
+      num % i == 0
       true
     end
   end
