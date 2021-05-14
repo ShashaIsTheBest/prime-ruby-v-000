68206cc3fea090e73977d83a856ad83ba4c31e37
Shasha Yan <shashayan@gmail.com>
2021-05-14 13:01:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
-# Add  code here!
+def prime?(number)# Add  code here!
+  if number < 0 || number == 0 || number == 1
+    return false 
+  else 
+    |number| % |!number| != 1  
+    return true
+  end
+end
+  
